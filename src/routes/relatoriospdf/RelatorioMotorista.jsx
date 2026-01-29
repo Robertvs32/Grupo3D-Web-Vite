757c6f03b577a07e95b0870804a3bca43d20e6cf
Robert <vs.robert4@gmail.com>
2026-01-29 15:54:26 -0300

    aasd

---
+++ src/routes/relatoriospdf/RelatorioMotorista.jsx
@@ -0,0 +1,210 @@
+import './relatorioContratante.css';
+import { useEffect, useMemo, useState } from 'react';
+import { Link } from 'react-router';
+import RelatorioContratantePDF from './relatorioContratantePDF';
+import { PDFViewer } from '@react-pdf/renderer';
+import { useLocation } from 'react-router';
+import usePdf from '../../hooks/usePdf';
+
+export default function RelatorioMotorista(){
+
+    const location = useLocation();
+    const [arrayRelatorios, setArrayRelatorios] = useState([]);
+    const [placas, setPlacas] = useState([]);
+    const [total, setTotal] = useState(0);
+    const { buscaRelatoriosPdf } = usePdf();
+    const [flag, setFlag] = useState(false);
+
+    /* 
+        NA PRIMEIRA RENDERIZACAO DO COMPONENTE
+        RECEBE OS DADOS DO NAVIGATION (ARRAY DE IDS)
+        BUSCA OS RELATORIOS DESSES IDS
+        ATUALIZA O ESTADO DE RELATORIOS (DADOS)
+        ATUALIZA O ESTADO DE PLACAS (SEM DUPLICIDADE E JA COM VALORES PARA PREENCHER)
+    */
+    useEffect(() => {
+        const ids = location.state;
+
+        if(ids && ids.length != 0){
+            const busca = async () => {
+                const arrayTemp = await buscaRelatoriosPdf(ids);
+                setArrayRelatorios(arrayTemp);
+                atualizaPlacaComValores(arrayTemp)
+            }
+            busca();
+        }
+    }, [])
+
+    useEffect(() => {
+        atualizaValorTotal(arrayRelatorios)
+    }, [placas])
+
+
+    //FUNCAO PARA ATUALIZAR O STATE DE PLACAS UTILIZADAS SEM DUPLICIDADE E JA COM OS CAMPOS DE VALORES
+    function atualizaPlacaComValores(relatorio){
+        const arraySomentePlacas = relatorio.map((item) => {
+            return item.placa;
+        })
+
+        const arraySemDuplicados = [...new Set(arraySomentePlacas)];
+
+        const arrayPlacaComValores = arraySemDuplicados.map((item) => {
+            return {placa: item, valorHora: 0, valorKm: 0}
+        })
+
+        setPlacas(arrayPlacaComValores)
+    }
+
+    //FUNCAO PARA ALTERAR OS VALORES DE UMA PLACA COM BASE EM UM CAMPO ESPECIFICO
+    function alteraValue(event, placa, campo){
+        let value = event.target.value;
+        
+        const arrayModificado = placas.map((item) => {
+            if(item.placa == placa){
+                return({...item, [campo]: value.replace(',', '.')});
+            }
+            return({...item})
+        })
+        
+        const arrayVerificado = verificaValoresPlacas(arrayModificado);
+
+        setPlacas(arrayVerificado);
+    }
+
+    //FUNCAO PARA BUSCAR O VALOR DO ARRAY PLACAS COM BASE EM UM CAMPO ESPECIFICO
+    function buscaValorPlaca(placa, campo){
+        const objetoPlaca = placas.find((item) => {
+            return item.placa == placa;
+        });
+        if(objetoPlaca){
+            return objetoPlaca[campo];
+        }
+        return 0;
+    }
+
+    //FUNCAO PARA ATUALIZAR O VALOR TOTAL, PERCORRENDO OS RELATORIOS E BUSCANDO OS VALORES DAS PLACAS UTILIZADAS
+    function atualizaValorTotal(arrayRelatorios){
+        const valorTotal = arrayRelatorios.reduce((contador, item) => {
+            // busca os valores por horas trabalhadas e kms rodados
+            const valorHora = Number(item.horasTrabalhadas) * Number(buscaValorPlaca(item.placa, 'valorHora'));
+            const valorKm = Number(item.kmRodado) * Number(buscaValorPlaca(item.placa, 'valorKm'));
+
+            let calculoViagem = 0;
+            
+            //atribui o valor das horas trabalhadas
+            calculoViagem += Number(valorHora.toFixed(2));
+
+            //se a viagem foi fora de perimetro, adiciona tambem o valor de kms
+            //Adicionar campo de perimetro no relatorio para usar aqui
+            if(true){
+                calculoViagem += Number(valorKm.toFixed(2))
+            };
+
+            console.log(`calculo viagem: ${calculoViagem}`)
+            return contador + calculoViagem;
+            
+        }, 0)
+        setTotal(valorTotal)
+    }
+
+
+    //VERIFICA SE OS VALORES FORAM PREENCHIDOS COM NUMEROS E RETORNA UMA CONFIRMACAO SE ESTA TUDO CORRETO
+    function verificaValoresPlacas(arrayPlacas){
+
+        const placasTemp = arrayPlacas.map((item) => {
+            const verificaHora = isNaN(Number(item.valorHora));
+            const verificaKm = isNaN(Number(item.valorKm));
+
+            if(verificaHora && verificaKm){
+                alert("Digite um numero valido!")
+                return({...item, valorHora: 0, valorKm: 0})
+            }
+            
+            if(verificaHora){
+                alert("Digite um numero valido!")
+                return({...item, valorHora: 0})
+            }
+
+            if(verificaKm){
+                alert("Digite um numero valido!")
+                return({...item, valorKm: 0})
+            }
+
+            return({...item})
+        })
+
+        return placasTemp; 
+    }
+
+    //COMPONENTE DE RELATORIO DO CONTRATANTE UTILIZANDO O MEMO PARA EVITAR RENDERIZACOES DESNECESSARIAS
+    const relatorioContratanteMemo = useMemo(() => (
+        <PDFViewer id="pdfViewer">
+            <RelatorioContratantePDF
+                arrayRelatorios={arrayRelatorios}
+                placas={placas}
+                valorTotal={total}
+            />  
+        </PDFViewer>
+    ), [arrayRelatorios, flag])
+
+
+    //COMPONENTE PAGINA RELATORIO CONTRATANTE
+    return(
+        <div id="containerRelatorioContratante">
+
+            <h1 id="titleRelatorioContratante">Relatorio contratante</h1>
+
+            <Link id="linkColecoes" to="/colecoes">Retornar as colecoes</Link>
+
+        <div id="placasContainer">
+            {placas.map((item) => (
+                <div className="containerPlaca">
+                    <p className="nomePlaca">{item.placa}</p>
+
+                    <p className="labelPlaca">Valor hora</p>
+                    <input 
+                        className="inputValorPlaca"
+                        type="text"
+                        value={item.valorHora}
+                        onChange={(event) => alteraValue(event, item.placa, 'valorHora')}
+                    />
+
+                    <p className="labelPlaca">Valor km</p>
+                    <input 
+                        className="inputValorKm"
+                        type="text"
+                        value={item.valorKm}
+                        onChange={(event) => alteraValue(event, item.placa, 'valorKm')}
+                    />
+                </div>
+            ))}
+        </div>
+            
+
+            <div>
+
+                <p style={{marginRight: '10px', color: 'white', fontWeight: 'bold'}}>Valor total</p>
+
+                <input 
+                    id="inputTotal"
+                    type="text"
+                    value={total}
+                    onChange={(event) => setTotal(event.target.value.replace(',', '.'))}
+                />
+            </div>
+            
+
+            <button 
+                id="btnAtualizaPdf"
+                onClick={() => {
+                    setTotal(valor => Number(valor));
+                    setFlag(ant => !ant)
+                }}>
+                Atualizar Valores
+            </button>
+
+            {relatorioContratanteMemo}
+
+        </div>
+    )
+}
---
+++ src/routes/relatoriospdf/relatorioMotoristaPDF.jsx
@@ -0,0 +1,215 @@
+import { Document, Page, Text, Image, StyleSheet, Font, View} from '@react-pdf/renderer'
+import logo from '../../assets/img/logoPreta.png';
+import Sora from '../../assets/fonts/Sora-Regular.ttf'
+import SoraBold from '../../assets/fonts/Sora-Bold.ttf'
+
+export default function RelatorioMotoristaPDF({arrayRelatorios, placas, valorTotal}){
+
+Font.register({
+    family: 'Sora',
+    fonts: [
+        { src: Sora },
+        { src: SoraBold, fontWeight: 'bold' }
+    ]
+})
+
+function horasFormatadas(horas){
+    const intHoras = parseInt(horas);
+    const minutos = (horas - intHoras) * 60;
+
+    if(intHoras == 0){
+        return(`${minutos} minutos`);
+    }
+    else if(minutos == 0){
+        return(`${intHoras} horas`);
+    }
+    else{
+        return(`${intHoras} hora(s) e ${minutos.toFixed(0)} minuto(s)`);
+    }
+}
+
+    return(
+            <Document>
+                <Page size="A4" orientation="landscape" style={styles.page}>
+                    <Image
+                        src={logo}
+                        style={styles.logoImg}
+                    />
+
+                    <Text style={styles.title}>Relatorio contratante</Text>
+
+                    <View style={styles.containerPlacas}>
+                    {placas.map((item) => (
+                        <View style={styles.cardPlacas}>
+                            <Text style={styles.textPlaca}>{item.placa}</Text>
+                            <Text style={styles.textValor}>{`Valor hora R$: ${item.valorHora}`}</Text>
+                            <Text style={styles.textValor}>{`Valor Km R$: ${item.valorKm}`}</Text>
+                        </View>
+                    ))}
+                    </View>
+
+                    <View style={styles.containerInfos}>
+                        {arrayRelatorios.map((item) => (
+                            <View style={styles.containerCard}>
+
+                                <View style={styles.infoCard}>
+                                    <Text style={styles.titleInfo}>Motorista</Text>
+                                    <View style={styles.itemInfo}>
+                                        <Text>{item.motorista}</Text>
+                                    </View>
+                                </View>
+
+                                <View style={styles.infoCard}>
+                                    <Text style={styles.titleInfo}>Job</Text>
+                                    <View style={styles.itemInfo}>
+                                        <Text>{item.job}</Text>
+                                    </View>
+                                </View>
+
+                                <View style={styles.infoCard}>
+                                    <Text style={styles.titleInfo}>Placa</Text>
+                                    <View style={styles.itemInfo}>
+                                        <Text>{item.placa}</Text>
+                                    </View>
+                                </View>
+
+                                <View style={styles.infoCard}>
+                                    <Text style={styles.titleInfo}>Inicio</Text>
+                                    <View style={styles.itemInfo}>
+                                        <Text>{item.dateTimeIni.toDate().toLocaleString()}</Text>
+                                    </View>    
+                                </View>
+
+                                <View style={styles.infoCard}>
+                                    <Text style={styles.titleInfo}>Termino</Text>
+                                    <View style={styles.itemInfo}>
+                                        <Text>{item.dateTimeFim.toDate().toLocaleString()}</Text>
+                                    </View>
+                                </View>
+
+                                <View style={styles.infoCard}>
+                                    <Text style={styles.titleInfo}>Setor</Text>
+                                    <View style={styles.itemInfo}>
+                                        <Text>{item.setor}</Text>
+                                    </View>
+                                </View>
+
+                                <View style={styles.infoCard}>
+                                    <Text style={styles.titleInfo}>Atribuicao</Text>
+                                    <View style={styles.itemInfo}>
+                                        <Text>{item.atribuicao}</Text>
+                                    </View>
+                                </View>
+
+                                <View style={styles.infoCard}>
+                                    <Text style={styles.titleInfo}>Horas trabalhadas</Text>
+                                    <View style={styles.itemInfo}>
+                                        <Text>{horasFormatadas(item.horasTrabalhadas)}</Text>
+                                    </View>          
+                                </View>
+
+                                <View style={styles.infoCard}>
+                                    <Text style={styles.titleInfo}>Km rodado</Text>
+                                    <View style={styles.itemInfo}>
+                                        <Text>{item.kmRodado}</Text>
+                                    </View>
+                                </View>
+                                
+                            </View>
+                        ))}
+
+                    </View>
+
+                    <Text style={styles.valorTotal}>{`Valor total: R$${valorTotal.toFixed(2).replace('.', ',')}`}</Text>
+                    
+                </Page>
+            </Document>
+    )
+}
+
+const styles = StyleSheet.create({
+    page: {
+        padding: '20',
+        position: 'relative',
+        fontFamily: 'Sora',
+    },
+    title: {
+        fontWeight: 'bold',
+        fontSize: '26',
+        marginBottom: '30px',
+        marginTop: '15px',
+        borderLeft: '5px solid rgba(252, 107, 16, 1)',
+        paddingLeft: '10px',
+        backgroundColor: 'rgba(0, 0, 0, 0.78)',
+        width: '310px',
+        color: 'white',
+        paddingVertical: '5px'
+    },
+    logoImg: {
+        width: '90',
+        position: 'absolute',
+        right: '20',
+        top: '20'
+    },
+    containerInfos: {
+        marginTop: '15px'
+    },
+    containerCard: {
+        display: 'flex',
+        flexDirection: 'row',
+        height: '42px',
+        backgroundColor: 'rgba(196, 194, 194, 1)'
+    },
+    infoCard: {
+        display: 'flex',
+        flexDirection: 'column',
+        alignItems: 'center',
+        flex: 1,
+    },
+    titleInfo: {
+        fontSize: 8,    
+        backgroundColor: 'rgba(46, 45, 45, 1)',
+        width: '100%',
+        color: 'white',
+        textAlign: 'center',
+        fontWeight: 'bold',
+        borderRight: '1px solid white'
+    },
+    itemInfo: {
+        fontSize: 7,
+        display: 'flex',
+        alignItems: 'center',
+        justifyContent: 'center',
+        width: '100%',
+        padding: '3px',
+        flex: '1',
+        borderRight: '1px solid white'
+    },
+    textPlaca: {
+        fontSize: '9px',
+        fontWeigth: 'bold',
+        backgroundColor: 'rgba(46, 45, 45, 1)',
+        color: 'white',
+        padding: '3px',
+        marginBottom: '2px'
+    },
+    textValor: {
+        fontSize: '8px',
+        padding: '3px',
+    },
+    containerPlacas: {
+        display: 'flex',
+        flexDirection: 'row'
+    },
+    cardPlacas: {
+        backgroundColor: 'rgba(196, 194, 194, 1)',
+        width: '120px',
+        marginRight: '5px'
+    },
+    valorTotal: {
+        position: 'absolute',
+        bottom: '25px',
+        right: '25px'
+    }
+    
+})
